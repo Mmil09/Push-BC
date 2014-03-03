d63095d7b2b09972e369d4e9f9e56fc1b8f2bfdc
Daniel <daniel@daniel-SH67H.(none)>
2014-03-02 20:53:41 -0500

    cleaned assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,8 @@ Pushbc::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.compress = false
-  config.assets.debug = true
+
+  config.assets.debug = false
+  config.assets.compile = false
+
 end
