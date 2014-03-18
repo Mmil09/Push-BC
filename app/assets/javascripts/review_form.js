c6dd3df561946a17d164b396c7ea98d05ac8a514
Daniel <daniel@daniel-SH67H.(none)>
2014-03-18 14:11:17 -0400

    fixed review tests, added javascript word counter for reviews

---
+++ app/assets/javascripts/review_form.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+
+  var background_required_words = 150;
+  var instruction_required_words = 150;
+  var overall_required_words = 300;
+
+  var wordCount = function(text) {
+    text = text.replace(/(^\s*)|(\s*$)/gi,"");
+    text = text.replace(/[ ]{2,}/gi," ");
+    text = text.replace(/\n /,"\n");
+    var word_count = text.split(" ").length.toString();
+    return word_count;
+  }//end of function
+
+  var getText = function(textbox) {
+    var text = $(textbox).val();
+    return text;
+  }
+
+  var displayWordCount = function(text_area_id, word_count_id) {
+    var text = getText(text_area_id);
+    var word_count = wordCount(text);
+    $(word_count_id).text("Word Count: " + word_count);
+  }
+
+  var hasRequiredWords = function(text_area_id, required_words) {
+    var text = getText(text_area_id)
+    if (wordCount(text) >= required_words) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  var changeFontColor = function(text_area_id, word_count_id, required_words) {
+    if (hasRequiredWords(text_area_id, required_words)) {
+      $(word_count_id).css("color", "black")
+    } else {
+      $(word_count_id).css("color", "red")
+    }
+  }
+
+  var wordCounter = function(text_area_id, word_count_id, required_words) {
+    $(text_area_id).keyup(function() {
+      displayWordCount(text_area_id, word_count_id)
+      changeFontColor(text_area_id, word_count_id, required_words)
+    });
+  }
+
+  var displayErrors = function(text_area_id, error_id, word_count) {
+    if (!hasRequiredWords(text_area_id, word_count)) {
+      $(error_id).css("color", "red").text("You must have the required words.")
+      return 0
+    }
+    else {
+      $(error_id).text("")
+      return 1
+    }
+  }
+  
+  wordCounter("#background", "#background-wordcount", background_required_words);
+  wordCounter("#instruction", "#instruction-wordcount", instruction_required_words);
+  wordCounter("#overall", "#overall-wordcount", overall_required_words);
+
+
+  $("#review_form").submit(
+    function(event) {
+      errors = 0;
+      errors = errors + displayErrors("#background", "#background-error", background_required_words);
+      errors = errors + displayErrors("#instruction", "#instruction-error", instruction_required_words);
+      errors = errors + displayErrors("#overall", "#overall-error", overall_required_words);
+      
+      if (errors < 3) {
+        event.preventDefault();
+      }
+    }
+
+  ); //end of submit 
+
+}); //end document.ready
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,9 @@
 class Review < ActiveRecord::Base
 
+  BACKGROUND_WORDS = 150
+  INSTRUCTION_WORDS = 150
+  OVERALL_WORDS = 300
+  
   validates :rating, :background, :instruction, :overall, presence: true
   validates_inclusion_of :rating, in: 1..5
   validates_length_of :overall, minimum: 300
@@ -8,6 +12,7 @@ class Review < ActiveRecord::Base
   scope :review_exists?, ->(user_id, bootcamp_id) { where(user_id: user_id, bootcamp_id: bootcamp_id).count } 
   validate :reviewed_by_user?, before: :save
   validate :background_enough_words?, :instruction_enough_words?, :overall_enough_words?, before: :save
+
 
   def reviewed_by_user?
     errors.add(:bootcamp_id, "You have already reviewed this bootcamp.") if Review.review_exists?(self.user_id, self.bootcamp_id) > 0
@@ -18,18 +23,16 @@ class Review < ActiveRecord::Base
   end
 
   def background_enough_words?
-    enough_words?("background", 150) 
+    enough_words?("background", BACKGROUND_WORDS) 
   end
 
   def instruction_enough_words?
-    enough_words?("instruction", 150)
+    enough_words?("instruction", INSTRUCTION_WORDS)
   end
 
   def overall_enough_words?
-    enough_words?("overall", 248) 
+    enough_words?("overall", OVERALL_WORDS) 
   end
-
-
 
   def enough_words?(section, min_words)
     errors.add(:section, "#{section} must have at least #{min_words}") if self.word_count(self.send(section)) < min_words 
@@ -38,7 +41,5 @@ class Review < ActiveRecord::Base
   def word_count(section)
     section.split.size if section != nil
   end
-
-  
 
 end
